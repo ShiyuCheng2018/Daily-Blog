570b1ab7a4e4c0548d78ecb0f00ea9907a6ffb47
Shiyu Cheng <starkeyshiyu@gmail.com>
2019-12-28 15:03:59 -0700

    configured User deletion, exports.deleteUser

--- Node-API/routes/userRouter.js
+++ Node-API/routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {userById, allUsers, getUser, updateUser} = require("../controllers/userController");
+const {userById, allUsers, getUser, updateUser, deleteUser} = require("../controllers/userController");
 const {requireSignIn} = require("../controllers/authController");
 
 // Get router
@@ -8,6 +8,8 @@ const router = express.Router();
 router.get("/users", allUsers);
 router.get("/user/:userId",requireSignIn, getUser);
 router.put("/user/:userId", requireSignIn, updateUser);
+router.delete("/user/:userId", requireSignIn, deleteUser);
+
 
 // Any route containing :userId, our app will first execute userById()
 router.param("userId", userById);
