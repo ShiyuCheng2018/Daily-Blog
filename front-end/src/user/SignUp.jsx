74e0cf2ff4e24adfe7bc877669c7f3dea25b3a23
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-02-16 00:51:04 -0700

    configured sign up event and validation info shown

--- front-end/src/user/SignUp.jsx
+++ front-end/src/user/SignUp.jsx
@@ -7,19 +7,70 @@ class SignUp extends Component{
             name: "",
             email: "",
             password: "",
-            error: ""
+            error: "",
+            open: false
         }
     }
 
     handleChange = (name) => (event) =>{
+        this.setState({error:""});
         this.setState({[name]: event.target.value})
     };
 
+    clickSubmit = (event) => {
+      event.preventDefault();
+      const {name, email, password, open} = this.state;
+      const user = {
+          name,
+          email,
+          password
+      };
+
+      this.signUp(user)
+          .then(data=>{
+              if(data.err){
+                  this.setState({error: data.err})
+              }else {
+                  this.setState({
+                      error: "",
+                      name:"",
+                      email:"",
+                      password:"",
+                      open: true
+                  })
+              }
+          })
+    };
+
+    signUp = user =>{
+        return fetch('http://local:8080/signup', {
+            method: 'POST',
+            headers:{
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res=>{
+                return res.json()
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    };
+
+
+
     render(){
-        const {name, email, password} = this.state;
+        const {name, email, password, error, open} = this.state;
         return (
             <div className={'container'}>
                 <h2 className={'mt-5 mb-5'}>Sign up</h2>
+                {/*validation*/}
+                <div className={"alert alert-danger"} style={{display:error ? "":"none"}}>{error}</div>
+                <div className={"alert alert-info"} style={{display:open ? "":"none"}}>New account is successfully created!! Please Sign In.</div>
+                {/*end of validation*/}
+
                 <form>
                     <div className={'form-group'}>
                         <label className={'text-muted'} >Name</label>
@@ -31,9 +82,9 @@ class SignUp extends Component{
                     </div>
                     <div className={'form-group'}>
                         <label className={'text-muted'} >Password</label>
-                        <input type={'password'} className={'form-control'} onChange={this.handleChange('passWord')} value={password}/>
+                        <input type={'password'} className={'form-control'} onChange={this.handleChange('password')} value={password}/>
                     </div>
-                    <button className={'btn btn-raised btn-primary'}>Submit</button>
+                    <button className={'btn btn-raised btn-primary'} onClick={this.clickSubmit}>Submit</button>
                 </form>
             </div>
 
