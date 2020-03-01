e2ccadbf787f621dd1373b7053f88a28ae392a0d
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-03-01 14:20:07 -0700

    feat multiple defalt post imgs and fix short posts have dots'

--- front-end/src/post/Posts.jsx
+++ front-end/src/post/Posts.jsx
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import {list} from './apiPost';
+import DefaultPost_0 from "../images/postDefaults/postDefult_0.png";
+import DefaultPost_1 from "../images/postDefaults/postDefult_1.jpg";
+import DefaultPost_2 from "../images/postDefaults/postDefult_2.png";
+import DefaultPost_3 from "../images/postDefaults/postDefult_3.jpg";
+import DefaultPost_4 from "../images/postDefaults/postDefult_4.jpeg";
+import DefaultPost_5 from "../images/postDefaults/postDefult_5.png";
+import DefaultPost_6 from "../images/postDefaults/postDefult_6.png";
+import DefaultPost_7 from "../images/postDefaults/postDefult_7.png";
 import {Link} from "react-router-dom";
-// import DefaultProfile from '../images/user.png';
 
 class Posts extends Component{
     constructor(){
@@ -9,9 +16,11 @@ class Posts extends Component{
         this.state = {
             posts: []
         }
+
     }
 
     componentDidMount() {
+
         list().then(data => {
             if(data.err){
                 console.log(data.err)
@@ -20,29 +29,33 @@ class Posts extends Component{
             }
         }).catch(err => {
             console.log(err);
-        })
+        });
+
     }
 
     renderPosts = posts => {
+        let DefaultPost = [DefaultPost_0, DefaultPost_1, DefaultPost_2, DefaultPost_3, DefaultPost_4, DefaultPost_5,
+            DefaultPost_6, DefaultPost_7];
         return (
             <div className="row">
                 {posts.map((post, i) => {
                     const postId = post.postedBy ? `/user/${post.postedBy._id}` : "";
                     const author = post.postedBy ? post.postedBy.name : "anonymous";
 
-                    return ( <div className="card col-md-3 mx-md-4 my-2" key={i}>
-                            {/*<img src={`${process.env.REACT_APP_API_URL}/user/photo/${user._id}`}*/}
-                            {/*     className="card-img-top" alt={user.name}*/}
-                            {/*     style={{ height: "250px", width: "250px" }}*/}
-                            {/*     onError={i => (i.target.src = `${DefaultProfile}`)}*/}
-                            {/*/>*/}
+                    return ( <div className="card col-md-4 my-2" key={i} style={{height: "600px"}}>
+                            <img src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
+                                 className="card-img-top img-thumbnail" alt={post.title}
+                                 style={{height: "300px", width:"auto"}}
+                                 onError={i => (i.target.src = `${DefaultPost[Math.floor(Math.random() * 7)]}`)}
+                            />
                             <div className="card-body">
                                 <h5 className="card-title">{post.title}</h5>
-                                <p className="card-text">{post.body.substring(0, 100)}</p>
+                                {post.body.length > 250 ? <p className="card-text">{post.body.substring(0, 250)} ...</p> :
+                                    <p className="card-text">{post.body}</p>}
                             </div>
-                            <p className={"font-italic mark"}>
-                                <Link to={postId} > {author} </Link>
-                                <span className={"text-right"}>
+                            <p className={"font-italic mark text-right"}>
+                                <Link to={postId} > {author}  </Link>
+                                <span>
                                     {new Date(post.created).toLocaleDateString("en-US")}
                                 </span>
                             </p>
