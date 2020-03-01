bd76fc27ae0f1ba83a4794429ef736c6c0b74fb3
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-03-01 00:44:43 -0700

    fixed getPosts method, and it sends posts array now

--- Node-API/controllers/userController.js
+++ Node-API/controllers/userController.js
@@ -45,24 +45,6 @@ exports.getUser = (req,res) => {
     return res.json(req.profile);
 };
 
-// exports.updateUser = (req, res, next) => {
-//   let user = req.profile;
-//   console.log(user);
-//   user = _.extend(user, req.body);  // Extend - mutate the source object
-//   user.updated = Date.now();
-//   user.save((err) => {
-//       if(err){
-//           return res.status(400).json({
-//               error: "You are not authorized to perform this action."
-//           })
-//       }
-//
-//       user.hashed_password = undefined;
-//       user.salt = undefined;
-//       res.json({user});
-//   });
-// };
-
 exports.updateUser = (req, res, next) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
