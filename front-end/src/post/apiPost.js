1829bb5354c2f2c6221f0c29472a147810fa3bab
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-03-07 21:06:32 -0700

    configured like/unlike methods in apiPost.jsx

--- front-end/src/post/apiPost.js
+++ front-end/src/post/apiPost.js
@@ -72,10 +72,45 @@ export const updatePost = (postId, token, post) => {
             Accept: "application/json",
             Authorization: `Bearer ${token}`
         },
-        body: post
+        body: post // DataFrame
     })
         .then(response => {
             return response.json();
         })
         .catch(err => console.log(err));
 };
+
+// Like / Unlike
+export const like = (userId, token, postId) => {
+    return fetch(`${process.env.REACT_APP_API_URL}/post/like`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({userId, postId})
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const unlike = (userId, token, postId) => {
+    return fetch(`${process.env.REACT_APP_API_URL}/post/unlike`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({userId, postId})
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
