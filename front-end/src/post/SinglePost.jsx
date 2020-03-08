3ee14187a131f4879ccfba2dedc85c4460ae9e1d
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-03-08 00:27:20 -0700

    configured cancleLike&cancelUnLike methods in apiPost.jsx, and likeToggle&dislikeToggle methods in SinglePost.jsx

--- front-end/src/post/SinglePost.jsx
+++ front-end/src/post/SinglePost.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {singlePost, removePost} from "./apiPost";
+import {singlePost, removePost, like, unlike, cancelLike, cancelUnLike} from "./apiPost";
 import DefaultPost_0 from "../images/postDefaults/postDefult_0.png";
 import DefaultPost_1 from "../images/postDefaults/postDefult_1.jpg";
 import DefaultPost_2 from "../images/postDefaults/postDefult_2.png";
@@ -10,13 +10,21 @@ import DefaultPost_6 from "../images/postDefaults/postDefult_6.png";
 import DefaultPost_7 from "../images/postDefaults/postDefult_7.png";
 import {Link, Redirect} from "react-router-dom";
 import {isAuthenticated} from "../auth";
-import {remove} from "../user/apiUser";
 
 
 class SinglePost extends Component {
     state={
         post: "",
-        redirectHome: false
+        redirectHome: false,
+        like: false,
+        likes: 0,
+        unlike: false,
+        unlikes: 0,
+    };
+
+    checkLike = (like) =>{
+        const userId = isAuthenticated().user._id;
+        return like.indexOf(userId) !== -1;
     };
 
     componentDidMount = () =>{
@@ -25,7 +33,12 @@ class SinglePost extends Component {
            if(data.error){
                console.log(data.error)
            } else {
-               this.setState({post: data})
+               this.setState({post: data,
+                   likes: data.likes.length,
+                   like:this.checkLike(data.likes),
+                   unlikes: data.unlikes.length,
+                   unlike: this.checkLike(data.unlikes),
+               })
            }
         });
     };
@@ -45,11 +58,52 @@ class SinglePost extends Component {
             });
     };
 
+    likeToggle = () => {
+        let callApi = this.state.like ? cancelLike : like;
+        const userId = isAuthenticated().user._id;
+        const postId = this.state.post._id;
+        const token = isAuthenticated().token;
+        callApi(userId, token, postId)
+            .then(data=>{
+               if(data.err){
+                   console.log(data.err);
+               } else {
+                   this.setState({
+                       like: !this.state.like,
+                       likes: data.likes.length,
+                       unlike: false,
+                       unlikes: data.unlikes.length,
+                   })
+               }
+            });
+    };
+
+    disLikeToggle = () => {
+        let callApi = this.state.unlike ? cancelUnLike : unlike;
+        const userId = isAuthenticated().user._id;
+        const postId = this.state.post._id;
+        const token = isAuthenticated().token;
+        callApi(userId, token, postId)
+            .then(data=>{
+                if(data.err){
+                    console.log(data.err);
+                } else {
+                    this.setState({
+                        unlike: !this.state.unlike,
+                        unlikes: data.unlikes.length,
+                        likes: data.likes.length,
+                        like: false,
+                    })
+                }
+            });
+    };
+
     renderPost = (post) => {
         let DefaultPost = [DefaultPost_0, DefaultPost_1, DefaultPost_2, DefaultPost_3, DefaultPost_4, DefaultPost_5,
             DefaultPost_6, DefaultPost_7];
         const postId = post.postedBy ? `/user/${post.postedBy._id}` : "";
         const author = post.postedBy ? post.postedBy.name : "anonymous";
+        const {unlikes, likes} = this.state;
         console.log(post);
 
         return (
@@ -59,14 +113,24 @@ class SinglePost extends Component {
                      style={{height: "70%", width:"70%"}}
                      onError={i => (i.target.src = `${DefaultPost[Math.floor(Math.random() * 7)]}`)}
                 />
+
+
                 <div className="card-body">
                     <h5 className="card-title">{post.title}</h5>
                     <p className="card-text">{post.body}</p>
 
                 </div>
-                <p className={"font-italic mark text-right"}>
-                    <Link to={postId} > {author}  </Link>
-                    <span>
+
+                <p className={"font-italic mark"}>
+                    <span className={"text-left mr-2"} onClick={this.likeToggle} style={{cursor: "pointer"}}>
+                        {likes} Like
+                    </span>
+                    <span className={"text-left"} onClick={this.disLikeToggle} style={{cursor: "pointer"}}>
+                        {unlikes} Dislike
+                    </span>
+
+                    <span className={"ml-5"}>
+                        <Link to={postId} > {author}  </Link>
                         {new Date(post.created).toLocaleDateString("en-US")}
                     </span>
                 </p>
--- front-end/src/post/apiPost.js
+++ front-end/src/post/apiPost.js
@@ -82,7 +82,25 @@ export const updatePost = (postId, token, post) => {
 
 // Like / Unlike
 export const like = (userId, token, postId) => {
+    console.log(userId, postId);
     return fetch(`${process.env.REACT_APP_API_URL}/post/like`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({userId, postId})
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const cancelLike = (userId, token, postId) => {
+    console.log(userId, postId);
+    return fetch(`${process.env.REACT_APP_API_URL}/post/cancellike`, {
         method: "PUT",
         headers: {
             Accept: "application/json",
@@ -113,4 +131,19 @@ export const unlike = (userId, token, postId) => {
         .catch(err => console.log(err));
 };
 
+export const cancelUnLike = (userId, token, postId) => {
+    return fetch(`${process.env.REACT_APP_API_URL}/post/cancelunlike`, {
+        method: "PUT",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({userId, postId})
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
 
