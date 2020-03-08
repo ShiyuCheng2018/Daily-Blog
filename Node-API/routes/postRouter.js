d3fcf8d4f0df437c8a8a5095630ccb2a7a5939c6
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-03-08 16:04:35 -0700

    feat comment/uncomment in postRouter.js

--- Node-API/routes/postRouter.js
+++ Node-API/routes/postRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {createPostValidator, getErrors} = require("../validator");
 const {postById, getPosts, createPost, postsByUser, isPoster,updatePost, deletePost, postPhoto, singlePost,
-    like, unlike, cancelUnLike, cancelLike} = require("../controllers/postController");
+    like, unlike, cancelUnLike, cancelLike, comment, uncomment} = require("../controllers/postController");
 const {requireSignIn} = require("../controllers/authController");
 const {userById} = require("../controllers/userController");
 
@@ -13,6 +13,10 @@ router.put('/post/like', requireSignIn, like);
 router.put('/post/cancellike', requireSignIn, cancelLike);
 router.put('/post/unlike', requireSignIn, unlike);
 router.put('/post/cancelunlike', requireSignIn, cancelUnLike);
+
+// Comments
+router.put('/post/comment', requireSignIn, comment);
+router.put('/post/uncomment', requireSignIn, uncomment);
 
 router.get("/", getPosts);
 router.post("/post/new/:userId", requireSignIn, getErrors, createPost, createPostValidator);
