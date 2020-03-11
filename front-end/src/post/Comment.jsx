090f64cbbe71ca6194c6cdb4616d1ee4fcf65ff0
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-03-10 18:27:47 -0700

    Feat isvalid for the comment post in Comment.jsx

--- front-end/src/post/Comment.jsx
+++ front-end/src/post/Comment.jsx
@@ -5,34 +5,49 @@ import {isAuthenticated} from "../auth";
 
 class Comment extends Component{
     state = {
-        text: ''
+        text: '',
+        error:''
+    };
+
+    isValid = () =>{
+        const {text} = this.state;
+        if(!text.length > 0 || text.length > 250){
+            this.setState({error: "Comment should not be empty and less than 250 characters long ..."});
+            return false;
+        }
+        return true
     };
 
     handleChange = event =>{
-        this.setState({text: event.target.value});
+        this.setState({text: event.target.value, error: ""});
     };
 
     addComment = (e) =>{
         e.preventDefault();
-        const userId = isAuthenticated().user._id;
-        const  token = isAuthenticated().token;
-        const postId = this.props.postId;
-        const comment_text = {text: this.state.text};
-        comment(userId, token, postId, comment_text)
-            .then(data=>{
-                if(data.error){
-                    console.log(data.error);
-                }else {
-                    this.setState({text: ''});
-                    // Dispatch list of comments to parent (SinglePost.jsx"
-                    this.props.updateComments(data.comments);
-                }
-            })
+        if(this.isValid()){
+            const userId = isAuthenticated().user._id;
+            const  token = isAuthenticated().token;
+            const postId = this.props.postId;
+            const comment_text = {text: this.state.text};
+            comment(userId, token, postId, comment_text)
+                .then(data=>{
+                    if(data.error){
+                        console.log(data.error);
+                    }else {
+                        this.setState({text: ''});
+                        // Dispatch list of comments to parent (SinglePost.jsx"
+                        this.props.updateComments(data.comments);
+                    }
+                })
+        }
+
     };
 
     render() {
+        const {error} = this.state;
         return (
             <div >
+                <div className={"alert alert-danger"} style={{display: error ? "" : "none"}}>{error}</div>
                 <form onSubmit={this.addComment}>
                     <div className={"form-group"}>
                         <input type={"text"} onChange={this.handleChange}
